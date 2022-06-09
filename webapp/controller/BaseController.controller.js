850b3580f39fb20befdfa64a2f68b901146b3c4a
JordanChavez21 <jordanchavez151@gmail.com>
2022-06-09 22:13:06 +0000

    Commit 3

--- webapp/controller/BaseController.controller.js
+++ webapp/controller/BaseController.controller.js
@@ -39,7 +39,7 @@ sap.ui.define([
     // just this.getResoureBundle() ... 
     getResourceBundle: function () {
  
-      // ... instead ofs
+      // ... instead ofss
       return this.getOwnerComponent().getModel("i18n").getResourceBundle();
  
     },
